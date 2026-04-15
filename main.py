ae12b25af69a24c76a8947d53beaa81563f1fb08
andyyin815-beep <andy.yin815@gmail.com>
2026-04-15 16:31:15 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,43 +1,36 @@
 import requests
 import time
+import urllib3
 
-TOKEN = "ZMvaknwB2/EU4PPjVhls/DCb8dITxVZjDLtbArfPVskXt6unAXNSpQOc1Rv7V/C7rc5QHaOW7lzKSPsBH4t730 tFj6492Gea9+caOScXpU1eHUHrJOa4tcbWdhlJ8l06PEpY1Y71xcI0oYZBeRqk5QdB04t89/1O/w1cDnyilFU="
-USER_ID = "Ue4ac469ed010e1cebba684c8cb399ae5"
+# 關閉 SSL 警告（很重要）
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-def send_line(msg):
-    url = "https://api.line.me/v2/bot/message/push"
+# 目標股票（台積電）
+stock_id = "2330"
+
+def get_stock_price():
+    url = f"https://mis.twse.com.tw/stock/api/getStockInfo.jsp?ex_ch=tse_{stock_id}.tw"
+
     headers = {
-        "Authorization": f"Bearer {TOKEN}",
-        "Content-Type": "application/json"
+        "User-Agent": "Mozilla/5.0",
+        "Referer": "https://mis.twse.com.tw/"
     }
-    data = {
-        "to": USER_ID,
-        "messages": [{"type": "text", "text": msg}]
-    }
-    requests.post(url, headers=headers, json=data)
 
-def check_stock():
-    url = "https://mis.twse.com.tw/stock/api/getStockInfo.jsp?ex_ch=tse_2330.tw"
-    res = requests.get(url).json()
-    price = float(res["msgArray"][0]["z"])
-    return price
-
-last_price = 0
-
-while True:
     try:
-        price = check_stock()
-        print("目前價格:", price)
+        res = requests.get(url, headers=headers, verify=False, timeout=10)
+        data = res.json()
 
-        if last_price != 0:
-            change = (price - last_price) / last_price * 100
-
-            if change > 2:
-                send_line(f"🚀 飆股警報！上漲 {round(change,2)}% 現價 {price}")
-
-        last_price = price
-        time.sleep(60)
+        if data.get("msgArray"):
+            price = data["msgArray"][0].get("z", "無成交")
+            print(f"目前價格: {price}")
+        else:
+            print("抓不到資料")
 
     except Exception as e:
         print("錯誤:", e)
-        time.sleep(60)
+
+
+# 每60秒抓一次
+while True:
+    get_stock_price()
+    time.sleep(60)
